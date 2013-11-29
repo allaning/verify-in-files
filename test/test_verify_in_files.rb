2af2c583574ab534a3702b3494105ea96f5aaf47
Allan Ing <xeero@yahoo.com>
2013-11-28 21:40:19 -0600

    Minor update to JSON test

--- test/test_verify_in_files.rb
+++ test/test_verify_in_files.rb
@@ -6,6 +6,16 @@ module VerifyInFiles
 
       class TestVerify < Test::Unit::TestCase
 
+        def test_parse_json
+          vif = Verifier.new
+          top = vif.read_checks_and_rules "test/data/simple_rules.json"
+          assert_not_equal( nil, top )
+
+#          lines = Util.get_file_as_array( $LOREM_IPSUM )
+#          top.run( lines )
+#          assert_equal(true, top.result)
+        end
+
         def test_parse_yaml
           vif = Verifier.new
           top = vif.read_checks_and_rules "test/data/simple_rules.yml"
